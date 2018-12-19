3a5bfb7783509e4a5bc0548af23621757a9403e5
ivosdc <ibozic@mail.de>
2018-12-19 12:46:57 +0100

    Enables Spring boot app DemoController and tests

---
+++ src/apiTest/java/ivosdc/DemoControllerTest.java
@@ -0,0 +1,36 @@
+package ivosdc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class DemoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getHelloWorld_returnsOk() throws Exception {
+        mockMvc.perform(get("/api/demo/helloworld"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getHelloWorld_returnsHelloWorld() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/demo/helloworld"))
+                .andReturn();
+
+        assertEquals(result.getResponse().getContentAsString(), "Hello World");
+    }
+}
---
+++ src/main/java/ivosdc/DemoController.java
@@ -0,0 +1,21 @@
+package ivosdc;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+@Api(tags = "Demo")
+public class DemoController {
+    DemoController() {
+    }
+
+    @GetMapping("/helloworld")
+    @ApiOperation(value = "Gets a HelloWorld.")
+    public String getHelloWorld() {
+        return "Hello World";
+    }
+}
